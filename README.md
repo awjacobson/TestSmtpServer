9942b809326640e7cc09cbf6bee2aa734c7f33c5
Aaron Jacobson <awjacobson@aaronjacobson.com>
2022-01-19 12:07:41 -0600

    Add project files.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Overview
+
+This is a simple SMTP server that can run in Visual Studio and allows the developer to set break point and inspect messages, etc.
+
+# Packages
+
+This project makes use of and builds on the following NuGet packages:
+
+* MimeKit
+* SmtpServer
+
+Other references:
+https://github.com/cosullivan/SmtpServer
---
+++ TestSmtpServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using SmtpServer;
+using SmtpServer.ComponentModel;
+using SmtpServer.Protocol;
+using SmtpServer.Storage;
+using System;
+using System.Buffers;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestSmtpServer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var options = new SmtpServerOptionsBuilder()
+                .ServerName("localhost")
+                .Port(25, 587)
+                .Build();
+
+            var serviceProvider = new ServiceProvider();
+            serviceProvider.Add(new SampleMessageStore());
+
+            var smtpServer = new SmtpServer.SmtpServer(options, serviceProvider);
+            await smtpServer.StartAsync(CancellationToken.None);
+        }
+    }
+
+    internal class SampleMessageStore : MessageStore
+    {
+        public override async Task<SmtpResponse> SaveAsync(ISessionContext context, IMessageTransaction transaction, ReadOnlySequence<byte> buffer, CancellationToken cancellationToken)
+        {
+            await using var stream = new MemoryStream();
+
+            var position = buffer.GetPosition(0);
+            while (buffer.TryGet(ref position, out var memory))
+            {
+                await stream.WriteAsync(memory, cancellationToken);
+            }
+
+            stream.Position = 0;
+
+            var message = await MimeKit.MimeMessage.LoadAsync(stream, cancellationToken);
+            Console.WriteLine(message.Attachments);
+            Console.WriteLine(message.TextBody);
+
+            return SmtpResponse.Ok;
+        }
+    }
+}
